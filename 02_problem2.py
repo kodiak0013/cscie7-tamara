cea658a7ad4049a2ae5b7300ba7a818e34ad05d7
tamara <kodiak0013@yahoo.com>
2020-02-12 22:05:04 -0500

    02_problem2.py

---
+++ 02_problem2.py
@@ -0,0 +1,14 @@
+# Homework 02_problem2
+# Exercise to declare variables, use if and else statements
+
+# store numbers in variables called a and b
+a = 3
+b = 8
+
+# test if a is greater than b
+# if it is, display 'a is greater than b'
+# if not, display 'a is not greater than b'
+if a > b:
+	print('a is greater than b')
+else:
+	print('a is not greater than b')
