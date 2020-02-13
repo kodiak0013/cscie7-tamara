12ecc3da73878cf272bf28bd263faca78f996e82
tamara <kodiak0013@yahoo.com>
2020-02-12 22:06:31 -0500

    02_problem3.py

---
+++ 02_problem3.py
@@ -0,0 +1,18 @@
+# Homework 02_problem3
+# Exercise to declare variables, use if, elif, and else statements
+
+# store numbers in variables called a and b
+a = 3
+b = 8
+
+# test if a is greater than b
+# if it is, display 'a is greater than b'
+# if not, test if a is less than b
+# if it is, display 'a is smaller than b
+# if not, display 'a is equal to b'
+if a > b:
+	print('a is greater than b')
+elif a < b:
+	print('a is smaller than b')
+else:
+	print('a is equal to b')
