12ddbe0dee8893edb5a616f466e2169e9433ee8a
tamara <kodiak0013@yahoo.com>
2020-02-20 00:16:22 -0500

    03_problem4.py

---
+++ 03_problem4.py
@@ -0,0 +1,19 @@
+# Homework 03_problem4
+
+# store tuple in variable called my_tuple and populate with elements 'tamara', 'blackpanther', and 'wilmart'
+my_tuple = ('tamara', 'blackpanther', 'wilmart')
+
+# store tuple in variable called new_tuple and populate with contents of my_tuple
+new_tuple = my_tuple
+
+# set new_tuple equal to my_tuple index 0
+new_tuple = my_tuple[0]
+
+# set new_tuple equal to my_tuple last index
+new_tuple = my_tuple[-1]
+
+# set new_tuple equal to my_tuple indices 1 through 2
+new_tuple = my_tuple[1:3]
+
+# set new_tuple equal to my_tuple start of index through end of index excluding last element
+new_tuple = my_tuple[-3:-1]
