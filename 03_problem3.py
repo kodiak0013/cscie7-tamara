11835e6dbb4efe354df44381e121634077933ca6
tamara <kodiak0013@yahoo.com>
2020-02-20 00:14:22 -0500

    03_problem3.py

---
+++ 03_problem3.py
@@ -0,0 +1,34 @@
+# Homework 03_problem3
+
+# store list in variable called my_list and populate with elements 'tamara', 'blackpanther', and 'wilmart'
+my_list = ['tamara', 'blackpanther', 'wilmart']
+
+# store list in variable called new_list and populate with contents of my_list
+new_list = my_list
+
+# set new_list equal to my_list indices 0 through 2
+new_list = my_list[0:3]
+
+# set new_list equal to my_list indices 0 through end of index
+new_list = my_list[0:]
+
+# set new_list equal to my_list start of index through index 2
+new_list = my_list[:3]
+
+# set new_list equal to my_list all indices
+new_list = my_list[:]
+
+# set new_list equal to my_list index 0
+new_list = my_list[0]
+
+# set new_list equal to my_list indices 0 through 1
+new_list = my_list[0:2]
+
+# set new_list equal to my_list indices 1 through end of index
+new_list = my_list[1:]
+
+# set new_list equal to my_list last index
+new_list = my_list[-1]
+
+# set new_list equal to my_list start of index through end of index excluding last element
+new_list = my_list[:-1]
