f118940038468ba44b67de910f98962e76f49dde
tamara <kodiak0013@yahoo.com>
2020-02-20 00:11:24 -0500

    03_problem2.py

---
+++ 03_problem2.py
@@ -0,0 +1,15 @@
+# Homework 03_problem2
+
+# store number in variables called a and b and set their values to 1 and 3 respectively
+a = 1
+b = 3
+
+# for each variable i set to value of range from 0 to 2, execute body of loop
+#   set a equal to value of b
+#   set b equal to value of a plus 1
+for i in range(3):
+    a = b
+    b = a + 1
+
+#   display 'What are the values of a and b?'
+print('What are the values of a and b?')
