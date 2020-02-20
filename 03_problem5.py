adedcd1a3fadafd403fe664f1698473d1a66fb49
tamara <kodiak0013@yahoo.com>
2020-02-20 00:18:25 -0500

    03_problem5.py

---
+++ 03_problem5.py
@@ -0,0 +1,19 @@
+# Homework 03_problem5
+
+# store set in variable called my_set and populate with elements 'Homer', and 'Marge'
+my_set = {'Homer','Marge'}
+
+# add 'Bart' to my_set
+my_set.add('Bart')
+
+# add 'Lisa' to my_set
+my_set.add('Lisa')
+
+# add 'Maggie' to my_set
+my_set.add('Maggie')
+
+# add 'Bart' and 'Lisa' to my_set
+my_set.update(['Bart','Lisa'])
+
+#   display my_set
+print(my_set)
